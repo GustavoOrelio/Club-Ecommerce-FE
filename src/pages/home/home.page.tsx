e91002a599f58efad29baf7fdbf0b1afbc9742e7
Gustavo Orelio <oreliogustavo@gmail.com>
2025-04-06 23:23:02 -0300

    feat: add Categories Component

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,9 +1,10 @@
 import Header from '../../components/header/header.component'
-
+import Categories from '../../components/categories/categories.component'
 const HomePage = () => {
   return (
     <div>
       <Header />
+      <Categories />
     </div>
   )
 }
