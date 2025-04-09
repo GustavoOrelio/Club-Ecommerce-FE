569a9a30bd3c094b58933370c66d49a3158cf28e
Gustavo Orelio <oreliogustavo@gmail.com>
2025-04-09 14:48:26 -0300

    feat: add React Hookk Form validation to Login Page and Custom Input

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -1,6 +1,6 @@
 import { BsGoogle } from 'react-icons/bs'
 import { FiLogIn } from 'react-icons/fi'
-
+import { useForm } from 'react-hook-form'
 // Components
 import Header from '../../components/header/header.component'
 import CustomButton from '../../components/custom-button/custom-button.component'
@@ -10,6 +10,14 @@ import CustomInput from '../../components/custom-input/custom-input.component'
 import { LoginContainer, LoginContent, LoginHeadline, LoginInputContainer, LoginSubtitle } from './login.styles'
 
 const LoginPage = () => {
+  const { register, formState: { errors }, handleSubmit } = useForm()
+
+  const handleSubmitPress = (data: any) => {
+    console.log(data)
+  }
+
+  console.log(errors)
+
   return (
     <>
       <Header />
@@ -23,15 +31,15 @@ const LoginPage = () => {
 
           <LoginInputContainer>
             <p>E-mail</p>
-            <CustomInput type="email" placeholder="Digite seu e-mail" />
+            <CustomInput hasError={!!errors?.email} type="email" placeholder="Digite seu e-mail" {...register('email', { required: true })} />
           </LoginInputContainer>
 
           <LoginInputContainer>
             <p>Senha</p>
-            <CustomInput type="password" placeholder="Digite sua senha" />
+            <CustomInput hasError={!!errors?.password} type="password" placeholder="Digite sua senha" {...register('password', { required: true })} />
           </LoginInputContainer>
 
-          <CustomButton startIcon={<FiLogIn size={18} />}>Entrar</CustomButton>
+          <CustomButton startIcon={<FiLogIn size={18} />} onClick={() => handleSubmit(handleSubmitPress)()}>Entrar</CustomButton>
         </LoginContent>
       </LoginContainer>
     </>
