8a81f859c2d6ccab19a323d48cb63b8ac030610e
Gustavo Orelio <oreliogustavo@gmail.com>
2025-04-09 17:18:03 -0300

    chore: add firebase configuration

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAgjIFxEAUHQDgkKBzPIBH2iHy_ZPecs7Y',
+  authDomain: 'meuprojeto-bc5d9.firebaseapp.com',
+  projectId: 'meuprojeto-bc5d9',
+  storageBucket: 'meuprojeto-bc5d9.firebasestorage.app',
+  messagingSenderId: '463966629960',
+  appId: '1:463966629960:web:2de73edea7db03210ba87d'
+}
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
