8a5428dcbca56d2a5f38a810697fc2bf16c94b4c
Gustavo Orelio <oreliogustavo@gmail.com>
2025-04-09 22:09:10 -0300

    feat: add login

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -12,16 +12,39 @@ import InputErrorMessage from '../../components/input-error-message/input-error-
 // Styles
 import { LoginContainer, LoginContent, LoginHeadline, LoginInputContainer, LoginSubtitle } from './login.styles'
 
+// Utilities
+import {
+  AuthError,
+  AuthErrorCodes,
+  signInWithEmailAndPassword
+} from 'firebase/auth'
+import { auth } from '../../config/firebase.config'
+
 interface LoginForm {
   email: string
   password: string
 }
 
 const LoginPage = () => {
-  const { register, formState: { errors }, handleSubmit } = useForm<LoginForm>()
+  const { register, formState: { errors }, handleSubmit, setError } = useForm<LoginForm>()
 
-  const handleSubmitPress = (data: any) => {
-    console.log(data)
+  const handleSubmitPress = async (data: LoginForm) => {
+    try {
+      const userCredentials = await signInWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      )
+
+      console.log({ userCredentials })
+    } catch (error) {
+      const _error = error as AuthError
+
+      if (_error.code === AuthErrorCodes.INVALID_LOGIN_CREDENTIALS) {
+        setError('email', { type: 'mismatchEmail' })
+        setError('password', { type: 'mismatchPassword' })
+      }
+    }
   }
 
   return (
@@ -40,12 +63,14 @@ const LoginPage = () => {
             <CustomInput hasError={!!errors?.email} type="email" placeholder="Digite seu e-mail" {...register('email', { required: true, validate: (value) => validator.isEmail(value) })} />
             {errors?.email?.type === 'required' && <InputErrorMessage>O e-mail é obrigatório</InputErrorMessage>}
             {errors?.email?.type === 'validate' && <InputErrorMessage>Por favor, insira um e-mail válido</InputErrorMessage>}
+            {errors?.email?.type === 'mismatchEmail' && <InputErrorMessage>E-mail ou senha inválidos</InputErrorMessage>}
           </LoginInputContainer>
 
           <LoginInputContainer>
             <p>Senha</p>
             <CustomInput hasError={!!errors?.password} type="password" placeholder="Digite sua senha" {...register('password', { required: true })} />
             {errors?.password?.type === 'required' && <InputErrorMessage>A senha é obrigatória</InputErrorMessage>}
+            {errors?.password?.type === 'mismatchPassword' && <InputErrorMessage>E-mail ou senha inválidos</InputErrorMessage>}
           </LoginInputContainer>
 
           <CustomButton startIcon={<FiLogIn size={18} />} onClick={() => handleSubmit(handleSubmitPress)()}>Entrar</CustomButton>
