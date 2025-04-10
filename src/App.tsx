6ed41c8018294c63ad68e0db85386b34d3564840
Gustavo Orelio <oreliogustavo@gmail.com>
2025-04-10 11:33:04 -0300

    feat: ensure User Context is updated on onAuthStateChanged

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { onAuthStateChanged } from 'firebase/auth'
-import { FunctionComponent } from 'react'
+import { FunctionComponent, useContext } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { getDocs, collection, where, query } from 'firebase/firestore'
 
 // Pages
 import HomePage from './pages/home/home.page'
@@ -8,12 +9,26 @@ import LoginPage from './pages/login/login.page'
 import SignUpPage from './pages/sign-up/sign-up.page'
 
 // Utilities
-import { auth } from './config/firebase.config'
+import { auth, db } from './config/firebase.config'
+import { UserContext } from './contexts/user.context'
 
 const App: FunctionComponent = () => {
-  onAuthStateChanged(auth, (user) => {
-    console.log({ user })
+  const { isAuthenticated, loginUser, logoutUser } = useContext(UserContext)
+
+  onAuthStateChanged(auth, async (user) => {
+    const isSigningOut = isAuthenticated && !user
+    if (isSigningOut) {
+      logoutUser()
+    }
+
+    const isSigningIn = !isAuthenticated && user
+    if (isSigningIn) {
+      const querySnapshot = await getDocs(query(collection(db, 'users'), where('id', '==', user.uid)))
+      const userFromFirestore = querySnapshot.docs[0]?.data()
+      loginUser(userFromFirestore as any)
+    }
   })
+
   return (
     <BrowserRouter>
       <Routes>
