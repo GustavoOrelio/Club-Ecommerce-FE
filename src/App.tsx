4758d8c6d5b5e57908617d0e4a2e2e82cf1ef218
Gustavo Orelio <oreliogustavo@gmail.com>
2025-04-10 22:15:06 -0300

    feat: add Category Details page and Component

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import HomePage from './pages/home/home.page'
 import LoginPage from './pages/login/login.page'
 import SignUpPage from './pages/sign-up/sign-up.page'
 import ExplorePage from './pages/explore/explore.page'
+import CategoryDetailsPage from './pages/category-details/category-details.page'
 
 // Utilities
 import { auth, db } from './config/firebase.config'
@@ -45,6 +46,7 @@ const App: FunctionComponent = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/explore" element={<ExplorePage />} />
+        <Route path="/category/:id" element={<CategoryDetailsPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/sign-up" element={<SignUpPage />} />
       </Routes>
