7029639b3f096ede1f3292b778ebcbb992ecefea
Gustavo Orelio <oreliogustavo@gmail.com>
2025-04-11 12:59:36 -0300

    feat: add products count to cart

--- src/contexts/cart.context.tsx
+++ src/contexts/cart.context.tsx
@@ -5,6 +5,7 @@ import Product from '../types/product.types'
 interface ICartContext {
   isVisible: boolean
   productsTotalPrice: number
+  productsCount: number
   products: CartProduct[]
   toggleCart: () => void
   addProductToCart: (product: Product) => void
@@ -16,6 +17,7 @@ interface ICartContext {
 export const CartContext = createContext<ICartContext>({
   isVisible: false,
   productsTotalPrice: 0,
+  productsCount: 0,
   products: [],
   toggleCart: () => { },
   addProductToCart: () => { },
@@ -31,6 +33,12 @@ const CartContextProvider: FunctionComponent<{ children: React.ReactNode }> = ({
   const productsTotalPrice = useMemo(() => {
     return products.reduce((acc, currentProduct) => {
       return acc + currentProduct.price * currentProduct.quantity
+    }, 0)
+  }, [products])
+
+  const productsCount = useMemo(() => {
+    return products.reduce((acc, currentProduct) => {
+      return acc + currentProduct.quantity
     }, 0)
   }, [products])
 
@@ -78,7 +86,18 @@ const CartContextProvider: FunctionComponent<{ children: React.ReactNode }> = ({
   }
 
   return (
-    <CartContext.Provider value={{ isVisible, productsTotalPrice, products, toggleCart, addProductToCart, removeProductFromCart, increaseProductQuantity, decreaseProductQuantity }}>
+    <CartContext.Provider
+      value={{
+        isVisible,
+        productsTotalPrice,
+        productsCount,
+        products,
+        toggleCart,
+        addProductToCart,
+        removeProductFromCart,
+        increaseProductQuantity,
+        decreaseProductQuantity
+      }}>
       {children}
     </CartContext.Provider>
   )
