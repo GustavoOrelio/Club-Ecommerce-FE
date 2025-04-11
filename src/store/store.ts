176f4cf884483d1e4a3403b772d9c0b9e95e9480
Gustavo Orelio <oreliogustavo@gmail.com>
2025-04-11 20:23:29 -0300

    feat: add separate user actions file and redux typing

--- src/store/store.ts
+++ src/store/store.ts
@@ -5,4 +5,6 @@ import rootReducer from './root-reducer'
 
 const store = createStore(rootReducer, applyMiddleware(logger as any))
 
+export type RootState = ReturnType<typeof store.getState>
+
 export default store
